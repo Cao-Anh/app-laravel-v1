e8106297acecb1dc747647fa2468c1cfa6b3c4bd
dangcaoanh <dangcaoanh1998@gmail.com>
2025-03-22 17:51:28 +0700

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('users.index');
-// });
+
 
 use App\Http\Controllers\UserController;
-
+Route::get('/', [UserController::class, 'index'])->name('users.index');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
