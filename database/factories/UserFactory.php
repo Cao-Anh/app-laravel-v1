d1f4a9ba64bb4914c14e4072a13ae09ce996c50f
dangcaoanh <dangcaoanh1998@gmail.com>
2025-03-22 16:39:53 +0700

    create user 2

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,10 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->unique()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'description' => fake()->text(200), 
+            'role' => fake()->randomElement(['member', 'admin']), 
             'remember_token' => Str::random(10),
         ];
     }
