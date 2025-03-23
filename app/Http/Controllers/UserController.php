faae6b16c65b319c47ca371be5d1fb3560033bc6
dangcaoanh <dangcaoanh1998@gmail.com>
2025-03-24 02:14:29 +0700

    update login method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index() 
     {   
-        if (!Auth::check()) {
+        if (!Auth::user()) {
     
             return redirect()->route('login')->with('error', 'Bạn cần phải đăng nhập.');
         }
