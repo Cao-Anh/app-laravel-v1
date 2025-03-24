0596fff4642a05b0c59e46ef1a2c915067ec7861
dangcaoanh <dangcaoanh1998@gmail.com>
2025-03-24 15:55:04 +0700

    Revert "update index method"
    This reverts commit fbd682e1d537848d7c34e2673c2ffece07d94616.
    
    # Conflicts:
    #	app/Http/Controllers/UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,21 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    public function index() 
-    {   
-        if (!Auth::user()) {
-    
-            return redirect()->route('login')->with('error', 'Bạn cần phải đăng nhập.');
-        }
-    
+    public function index()
+    {
         $users = User::paginate(10);
         return view('users.index', compact('users'));
     }
-    
 
     public function show($id)
     {
