97e88ed768da5f3ec1cb8fa5adaf4574dbc37036
dangcaoanh <dangcaoanh1998@gmail.com>
2025-03-25 17:46:53 +0700

    add register function

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+
 
 Route::middleware('auth')->group(function () {
     // Route::get('/dashboard', function () {
