de52abfbc06205c1e7be7bc0734ac8185db96604
dangcaoanh <dangcaoanh1998@gmail.com>
2025-03-26 15:57:46 +0700

    fix remember feature

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->text('description')->nullable(); 
-            $table->string('role')->default('member');
+            $table->enum('role', ['admin', 'member'])->default('member');
             $table->rememberToken();
             $table->timestamps();
         });
